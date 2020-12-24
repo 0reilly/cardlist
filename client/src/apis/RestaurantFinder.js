8747740d1a242fff49b8bb3b1c20c563d79508f7
Adam OReilly <adamoreilly@Adams-MacBook-Pro.local>
2020-12-24 02:12:33 -0500

    edit

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "productroles.herokuapp.com/api/v1/restaurants"
+    baseURL: "http://productroles.herokuapp.com/api/v1/restaurants"
 });
