f647a1fd240706be366dbc0daf44362d3cde0452
Adam OReilly <adamoreilly@Adams-MacBook-Pro.local>
2021-01-08 18:36:51 -0500

    fixes

--- client/src/components/CheckoutForm.jsx
+++ client/src/components/CheckoutForm.jsx
@@ -53,7 +53,7 @@ const CheckoutForm= (props) => {
 const paymentIntent = async (e) => {
   e.preventDefault();
   window
-      .fetch("http://localhost:3006/create-payment-intent", {
+      .fetch("https://productroles.herokuapp.com/create-payment-intent", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
