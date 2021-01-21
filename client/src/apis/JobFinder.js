89de4457b3cd9c49734ea4f46c1b7015b8a50d40
Adam OReilly <adamoreilly@Adams-MacBook-Pro.local>
2021-01-21 18:46:18 -0500

    config vars

--- client/src/apis/JobFinder.js
+++ client/src/apis/JobFinder.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+const url = "";
 if(process.env.NODE_ENV === "production"){
     console.log("prod baseurl: "+process.env.BASE_URL)
-    export default axios.create({
-       
-        baseURL: process.env.BASE_URL 
-    });
+    url = process.env.BASE_URL;
 }
 else{
     export default axios.create({
         
-        baseURL: "http://localhost:3006/api/v1/jobs"
+        url = "http://localhost:3006/api/v1/jobs";
     });
 }
+export default axios.create({
+       
+    baseURL: url
+});
