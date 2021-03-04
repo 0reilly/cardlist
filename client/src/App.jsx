ffed4ae3baf1355edc9656a6c10fb50ca095f67a
Adam O'Reilly <oreilly.119@osu.edu>
2021-03-03 19:11:49 -0500

    merge fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,11 @@ import {Switch, Route} from "react-router-dom";
 import { BrowserRouter as Router, useParams } from 'react-router-dom';
 import { JobsContextProvider } from './context/JobContext';
 
-
 import Home from './routes/Home';
 import AddJob from './routes/AddJob';
 import JobDetailPage from "./routes/JobDetailPage";
 import PaymentSuccess from "./routes/PaymentSuccess";
-<<<<<<< HEAD
 import NewFeature from "./routes/NewFeature";
-=======
-import ReactGA from 'react-ga';
->>>>>>> bbe143071de3ce927adaaf2cae0cb7129b384e5d
 
 
 const App = () => {
